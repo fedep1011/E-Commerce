b8c2cfcfcc63068314c450bcec2bedd6f1de49e6
Federico Pagues <federicopagues@gmail.com>
2021-08-20 10:15:20 -0300

    Lista Productos Estilizada
    Se le dio formato al apartado de productos.

--- js/products.js
+++ js/products.js
@@ -1,18 +1,45 @@
-const showList = (productos) => {
-    const div = document.createElement("div");
-    const list = document.createElement("ul");
 
-    for (let prod of productos) {
-        const li = document.createElement("li"); 
-        li.appendChild(document.createTextNode(`${prod.name} ${prod.description} $${prod.cost}`));
-        list.appendChild(li);
+var productsArray = [];
 
+function showProductsList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let products = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ products.name +`</h4>
+                        <h5 class="text-muted">` + products.cost + products.currency + ` </h5>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("products-container").innerHTML = htmlContentToAppend;
+
+        
+    }
 }
-document.body.appendChild(div);
-div.appendChild(list);    
-}
 
-document.addEventListener("DOMContentLoaded", async function (e) {
-    const productos = (await getJSONData(PRODUCTS_URL)).data;
-    showList(productos)
-})
+
+document.addEventListener("DOMContentLoaded", function(){
+    showSpinner()
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productsArray = resultObj.data;
+            
+            showProductsList(productsArray);
+            hideSpinner()
+        }
+    });
+});
